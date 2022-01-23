08e7b98a52328a3e2a5a891555b9df3c8a765fc6
raoht <raohongtai@live.com>
2022-01-24 00:27:59 +0800

    8 readonly2

--- 8-medium-readonly-2/template.ts
+++ 8-medium-readonly-2/template.ts
@@ -1,5 +1,9 @@
-type MyReadonly2<T, K = keyof T> = {
-  readonly [P in keyof T as P extends K ? P : never]: T[P]
-} & {
-  [P in keyof T as P extends K ? never : P]: T[P]
+// type MyReadonly2<T, K = keyof T> = {
+//   readonly [P in keyof T as P extends K ? P : never]: T[P]
+// } & {
+//   [P in keyof T as P extends K ? never : P]: T[P]
+// }
+
+type MyReadonly2<T, K extends keyof T = keyof T> = {
+  readonly [P in K]: T[P] & Omit<T, K>
 }
