753bea100ec8fa00a1663a50befdfcae52300d8c
raoht <raohongtai@live.com>
2022-02-26 21:16:14 +0800

    none

--- 599-medium-merge/template.ts
+++ 599-medium-merge/template.ts
@@ -1,22 +1,22 @@
-type Merge<F, S> = {
-  [P in keyof (F & S)]: P extends keyof S
-    ? S[P]
-    : P extends keyof F
-    ? F[P]
-    : never
+// type Merge<T extends Record<any, any>, U extends Record<any, any>> = {
+//   [P in keyof (T & U)]: P extends keyof U
+//     ? U[P]
+//     : P extends keyof T
+//     ? T[P]
+//     : never
+// }
+
+type Merge<T extends Record<any, any>, U extends Record<any, any>> = {
+  [P in keyof (T & U)]: (T & U)[P] extends never ? U[P] : T[P]
 }
 
-// type Foo = {
-//   a: number
-//   b: string
-// }
-// type Bar = {
-//   b: number
-//   c: boolean
-// }
+type Foo = {
+  a: number
+  b: string
+}
+type Bar = {
+  b: number
+  c: boolean
+}
 
-// type dd = (Foo & Bar)['b']
-// (F & S)[key] extends never 说明类型冲突  取后面的一个类型
-// type Merge<F extends Record<any, any>, S extends Record<any, any>> = {
-//     [key in keyof (F & S)]: (F & S)[key] extends never ? S[key] : (F & S)[key];
-//   };
+type dc = Merge<Foo, Bar>
