cd2b0179a35988c6b7e002a2e4351fee999cce19
raoht <raohongtai@live.com>
2022-03-07 11:10:31 +0800

    none

--- 2757-medium-partialbykeys/template.ts
+++ 2757-medium-partialbykeys/template.ts
@@ -2,24 +2,29 @@ type IntersectionToObj<T> = {
   [P in keyof T]: T[P]
 }
 
-type PartialByKeys<T, K = keyof T> = IntersectionToObj<
+type PartialByKeys<T, U = keyof T> = IntersectionToObj<
   {
-    [P in keyof T as P extends K ? P : never]?: T[P]
-  } & { [P in keyof T as P extends K ? never : P]: T[P] }
+    [P in keyof T as P extends U ? never : P]: T[P]
+  } & {
+    [P in keyof T as P extends U ? P : never]?: T[P]
+  }
 >
-
-// interface User {
-//   name: string
-//   age: number
-//   address: string
-// }
-// type a = PartialByKeys<User, 'name'>
 
 // type Intersection<A, B> = A & B extends infer U
 //   ? { [P in keyof U]: U[P] }
 //   : never
 
-// type PartialByKeys<T, K = keyof T> = Intersection<
-//   Partial<Pick<T, Extract<K, keyof T>>>,
-//   Pick<T, Exclude<keyof T, K>>
-// >
+type Intersection<A, B> = { [P in keyof (A & B)]: (A & B)[P] }
+
+type PartialByKeys2<T, K = keyof T> = Intersection<
+  Partial<Pick<T, Extract<K, keyof T>>>,
+  Pick<T, Exclude<keyof T, K>>
+>
+
+interface User {
+  name: string
+  age: number
+  address: string
+}
+
+type cccc = PartialByKeys2<User, 'name'>
